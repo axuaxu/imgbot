5e0f221075f86763b9d0c60d67ed7af73cc48268
axuaxu <axu0110@gmail.com>
2017-09-12 21:31:46 -0400

    3

--- img_d1.py
+++ img_d1.py
@@ -17,14 +17,14 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
  
-ft = codecs.open("gt.txt","w",encoding="utf-16")
+ft = codecs.open("gt.txt","w",encoding="utf-8")
 ss = api.user_timeline(id='Amazing_Greece',count=1)
 for status in ss:
     #print status.author, status.user
     sid = status._json['id']
     #api.retweet(sid)
     #print status._json['media_url']
-    print status.text.encode('utf-16')
+    print status.text.encode('utf-8')
     #print status._json['entities']['media'][0]['media_url_https']
     #print status._json
     json.dump(status._json,ft)
