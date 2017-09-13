8dd8dac9a591b0a4900fe905ca00f5d3684c4ff6
axuaxu <axu0110@gmail.com>
2017-09-13 11:54:23 -0400

    3

--- img_greece.py
+++ img_greece.py
@@ -19,16 +19,28 @@ api = tweepy.API(auth)
 #4502
 twi_id = "Amazing_Greece"
 line = "" 
+iurl=''
+vurl=''
 ft = codecs.open("greece.txt","w",encoding="utf-8")
-ss = api.user_timeline(id=twi_id,count=100)
+ss = api.user_timeline(id=twi_id,count=1)
 for status in ss:
     #print status.author, status.user
     sid = status._json['id']
     stext = status.text.encode('utf-8')
     #api.retweet(sid)
     #print status._json['media_url']
-    surl = status._json['entities']['media'][0]['media_url_https']
-     
+    #surl = status._json['entities']['media'][0]['media_url_https']
+    #tweepy.Cursor(api.search, q="#hashtag", count=5, include_entities=True)
+    if 'media' in status.entities:
+        for image in  status.entities['media']:
+            iurl = image['media_url_https']
+    if 'media' in status.extended_entities:
+    	for video in status.extended_entities['media']:
+    		vurl = video['video_info']['variants'][1]['url']
+            #print iurl
     #print status._json
-    line = line + twi_id+','+sid+','+stext+','+surl+',\n'
+    line = line + twi_id+','+str(sid)+','+stext+','+iurl+','+vurl+'\n'
     #json.dump(status._json,ft)
+#print line
+#print surl
+ft.write(line)
